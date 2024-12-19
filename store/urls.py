541b0b4e71ffb9ed81e1f03ac3b8172c886d8c73
Veronika Surkova <surkova_nika92@mail.ru>
2024-12-19 19:13:27 +0300

    Fix urls

--- store/urls.py
+++ store/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
-from store.views import home, contacts, game_details, games_for_children, games_for_adults, games_for_families, \
-    games_all, add_game
+from store.views import HomeView, ContactFormView, GameDetailView, GamesForChildrenListView, GamesForAdultsListView, \
+    GamesForFamiliesListView, GameListView, GameCreateView, GameUpdateView, GameDeleteView
 from store.apps import StoreConfig
 
 app_name = StoreConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('game_details/<int:game_id>/', game_details, name='game_details'),
-    path('games_for_children/', games_for_children, name='games_for_children'),
-    path('games_for_adults/', games_for_adults, name='games_for_adults'),
-    path('games_for_families/', games_for_families, name='games_for_families'),
-    path('games_all/', games_all, name='games_all'),
-    path('add_game/', add_game, name='add_game'),
-    path('contacts/', contacts, name='contacts')
+    path('', HomeView.as_view(), name='home'),
+    path('game/<int:pk>/', GameDetailView.as_view(), name='game_details'),
+    path('games_for_children/', GamesForChildrenListView.as_view(), name='games_for_children'),
+    path('games_for_adults/', GamesForAdultsListView.as_view(), name='games_for_adults'),
+    path('games_for_families/', GamesForFamiliesListView.as_view(), name='games_for_families'),
+    path('games_all/', GameListView.as_view(), name='games_all'),
+    path('add_game/', GameCreateView.as_view(), name='add_game'),
+    path('game/<int:pk>/edit/', GameUpdateView.as_view(), name='update_game'),
+    path('game/<int:pk>/delete/', GameDeleteView.as_view(), name='delete_game'),
+    path('contacts/', ContactFormView.as_view(), name='contacts')
 ]
