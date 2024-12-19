fd7b32186a21976ad9f7dee1ba1933ad9513a976
Veronika Surkova <surkova_nika92@mail.ru>
2024-12-20 00:22:49 +0300

    Fix store app

--- store/views.py
+++ store/views.py
@@ -16,7 +16,7 @@ class HomeView(TemplateView):
 
 class GameDetailView(DetailView):
     model = Game
-    template_name = 'store/game_details.html'
+    template_name = 'store/blog_detail.html'
     context_object_name = "game"
 
 
@@ -30,30 +30,30 @@ class GamesForChildrenListView(ListView):
     model = Game
     template_name = 'store/for_children.html'
 
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        context_data['object_list'] = Game.objects.filter(category__title='Для детей')
-        return context_data
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(category__title='Для детей')
+        return queryset
 
 
 class GamesForAdultsListView(ListView):
     model = Game
     template_name = 'store/for_adults.html'
 
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        context_data['object_list'] = Game.objects.filter(category__title='Для взрослых')
-        return context_data
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(category__title='Для взрослых')
+        return queryset
 
 
 class GamesForFamiliesListView(ListView):
     model = Game
     template_name = 'store/for_families.html'
 
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        context_data['object_list'] = Game.objects.filter(category__title='Для всей семьи')
-        return context_data
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(category__title='Для всей семьи')
+        return queryset
 
 
 class ContactFormView(TemplateView):
@@ -66,19 +66,18 @@ class ContactFormView(TemplateView):
 class GameCreateView(CreateView):
     model = Game
     fields = ['title', 'description', 'image', 'category', 'price']
-    template_name = 'store/add_game.html'
+    template_name = 'store/blog_form.html'
     success_url = reverse_lazy('store:games_all')
 
 
 class GameUpdateView(UpdateView):
     model = Game
     fields = ['title', 'description', 'image', 'category', 'price']
-    template_name = 'store/add_game.html'
+    template_name = 'store/blog_form.html'
     success_url = reverse_lazy('store:games_all')
 
 
 class GameDeleteView(DeleteView):
     model = Game
-    fields = ['title', 'description', 'image', 'category', 'price']
-    template_name = 'store/delete_game.html'
+    template_name = 'store/blog_confirm_delete.html'
     success_url = reverse_lazy('store:games_all')
