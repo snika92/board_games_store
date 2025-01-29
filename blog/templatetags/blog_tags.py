eb190f70dcc728d469f5582203c21024ffa2a9de
Veronika Surkova <surkova_nika92@mail.ru>
2025-01-29 13:25:56 +0300

    Add templatetag 'has_group' to blog_tags and my_tags.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,3 +8,7 @@ def media_filter_blog(path):
     if path:
         return f"/media/{path}"
     return "#"
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
